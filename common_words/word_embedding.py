3dc7c70fe4cafd47baaa3c8dde15dea664a49588
Steven Samson <steven.a.samson@gmail.com>
2017-03-07 20:53:25 -0800

    Flushed out word embedding

--- common_words/word_embedding.py
+++ common_words/word_embedding.py
@@ -1,14 +1,19 @@
 import numpy as np
+from heapq import heappush, heapreplace
+
 
 class WordEmbedding:
-    def __init__(self, vector_file):
+
+    def __init__(self, vector_file, vector_length=100):
         self.entries = dict()
         with open(vector_file, 'r') as f:
             for line in f:
                 entry = line.split()
                 word = entry[0]
-                vector = np.array([float(index) for index in entry[1:]])
-                self.entries[word] = vector
+                vector = [float(index) for index in entry[1:]]
+                if len(vector) == vector_length:
+                    vector_array = np.array(vector)
+                    self.entries[word] = vector_array
 
     def __len__(self):
         return len(self.entries)
@@ -16,15 +21,35 @@ class WordEmbedding:
     def vocabulary(self):
         return self.entries.keys()
 
-    def cosine_similarity(self, word1, word2):
+    def find_vector(self, word, default=None):
+        return self.entries.get(word, default)
+
+    def cosine_similarity(self, vector1, vector2):
+        norm1 = np.linalg.norm(vector1)
+        norm2 = np.linalg.norm(vector2)
+        dot_product = np.dot(vector1, vector2)
+        return dot_product / (norm1 * norm2)
+
+    def word_similarity(self, word1, word2):
         if word1 in self.entries and word2 in self.entries:
             vector1 = self.entries[word1]
             vector2 = self.entries[word2]
-            norm1 = np.linalg.norm(vector1)
-            norm2 = np.linalg.norm(vector2)
-            dot_product = np.dot(vector1, vector2)
-            return dot_product / (norm1 * norm2)
+            return self.cosine_similarity(vector1, vector2)
         else:
             raise KeyError('one of the requested words, {} or {}, \
                     is not in the embedding'.format(word1, word2))
 
+    def find_k_nearest_neighbors(self, word, k=10):
+        if word in self.entries:
+            word_vector = self.entries[word]
+            knn = []
+            for other_word, other_vector in self.entries.items():
+                if other_word != word:
+                    score = self.cosine_similarity(word_vector, other_vector)
+                    if len(knn) >= k and score > knn[0][0]:
+                        heapreplace(knn, (score, other_word))
+                    elif len(knn) < k:
+                        heappush(knn, (score, other_word))
+            return [(w, s) for s, w in knn]
+        else:
+            raise KeyError('{} is not in the embedding'.format(word))
--- tests/test_word_embedding.py
+++ tests/test_word_embedding.py
@@ -1,21 +1,39 @@
 import unittest
 from common_words.word_embedding import WordEmbedding
 
+
 class WordEmbeddingTest(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.embedding = WordEmbedding('data/text9.vec')
 
     def test_length(self):
-        self.assertEqual(218317, len(self.embedding))
+        self.assertEqual(218316, len(self.embedding))
 
     def test_vocabulary(self):
-        self.assertEqual(218317, len(self.embedding.vocabulary()))
+        self.assertEqual(218316, len(self.embedding.vocabulary()))
         for word in self.embedding.vocabulary():
             self.assertTrue(word is not None)
 
+    def test_find_word_vector(self):
+        self.assertIsNot(None, self.embedding.find_vector('hello'))
+        self.assertIs(None, self.embedding.find_vector('supercalifragic'))
+
     def test_cosine_similarity(self):
-        self.assertAlmostEqual(1.0, self.embedding.cosine_similarity('hello', 'hello'), 2)
-        different_similarity = self.embedding.cosine_similarity('hello', 'world')
+        vector1 = self.embedding.find_vector('hello')
+        vector2 = self.embedding.find_vector('world')
+        self.assertAlmostEqual(1.0, self.embedding.cosine_similarity(vector1, vector1), 2)
+        different_similarity = self.embedding.cosine_similarity(vector1, vector2)
         self.assertTrue(different_similarity > 0.0 and different_similarity < 1.0)
 
+    def test_word_similarity(self):
+        self.assertAlmostEqual(1.0, self.embedding.word_similarity('hello', 'hello'), 2)
+        different_similarity = self.embedding.word_similarity('hello', 'world')
+        self.assertTrue(different_similarity > 0.0 and different_similarity < 1.0)
+
+    def test_find_k_nearest_neighbors(self):
+        knn = self.embedding.find_k_nearest_neighbors('meth', 10)
+        self.assertEqual(10, len(knn))
+        for word, score in knn:
+            self.assertNotEqual('meth', word)
+            self.assertTrue(score > 0.8)
